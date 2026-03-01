b2953b5805bfaf501f7a99ab96c4ef85ec0e4f47
Sean Peters <sean@snptrs.dev>
2026-03-01 10:30:55 +0000

    Clone main blog

---
+++ eleventy.config.js
@@ -0,0 +1,62 @@
+import pluginRss from "@11ty/eleventy-plugin-rss";
+import tailwindcss from "@tailwindcss/postcss";
+import { execSync } from "child_process";
+import cssnano from "cssnano";
+import fs from "fs";
+import markdownitFootnote from "markdown-it-footnote";
+import path from "path";
+import postcss from "postcss";
+
+export default function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addLiquidFilter("dateToRfc3339", pluginRss.dateToRfc3339);
+  eleventyConfig.addLiquidFilter(
+    "getNewestCollectionItemDate",
+    pluginRss.getNewestCollectionItemDate,
+  );
+
+  const processor = postcss([tailwindcss(), cssnano({ preset: "default" })]);
+
+  eleventyConfig.on("eleventy.before", async () => {
+    const tailwindInputPath = path.resolve("./src/assets/styles/index.css");
+    const tailwindOutputPath = "./_site/assets/styles/index.css";
+
+    const cssContent = fs.readFileSync(tailwindInputPath, "utf8");
+
+    const outputDir = path.dirname(tailwindOutputPath);
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    }
+
+    const result = await processor.process(cssContent, {
+      from: tailwindInputPath,
+      to: tailwindOutputPath,
+    });
+
+    fs.writeFileSync(tailwindOutputPath, result.css);
+  });
+
+  eleventyConfig.on("eleventy.after", () => {
+    execSync(`npx pagefind --site _site --glob "**/*.html"`, {
+      encoding: "utf-8",
+    });
+  });
+
+  // Tell the dev server to watch the compiled CSS and reload when it changes
+  eleventyConfig.setServerOptions({
+    watch: ["_site/assets/styles/**/*.css"],
+  });
+
+  eleventyConfig.amendLibrary("md", (mdLib) => {
+    mdLib.set({ typographer: true });
+    mdLib.use(markdownitFootnote);
+  });
+
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/assets/fonts");
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+
+  return {
+    dir: { input: "src", output: "_site" },
+  };
+}
