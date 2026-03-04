c8a7843c8a08279efef57dc207769a3495041262
Sean Peters <sean@snptrs.dev>
2026-03-04 22:09:02 +0000

    Change image config

--- eleventy.config.js
+++ eleventy.config.js
@@ -65,7 +65,7 @@ export default function (eleventyConfig) {
   });
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
-    formats: ["webp", "png", "jpeg", "avif"],
+    formats: ["avif", "webp", "png", "jpeg"],
     widths: ["auto"],
     outputDir: ".cache/@11ty/img/",
     urlPath: "/img/built/",
